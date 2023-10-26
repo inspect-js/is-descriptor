8bcf0288c53c80297e6109f7632dab9b7b7fb5c5
Jordan Harband <ljharb@gmail.com>
2023-10-26 10:43:17 -0700

    [eslint] actually use eslint

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*!
- * is-descriptor <https://github.com/jonschlinkert/is-descriptor>
- *
- * Copyright (c) 2015-2017, Jon Schlinkert.
- * Released under the MIT License.
- */
-
 'use strict';
 
 var typeOf = require('kind-of');
@@ -12,11 +5,11 @@ var isAccessor = require('is-accessor-descriptor');
 var isData = require('is-data-descriptor');
 
 module.exports = function isDescriptor(obj, key) {
-  if (typeOf(obj) !== 'object') {
-    return false;
-  }
-  if ('get' in obj) {
-    return isAccessor(obj, key);
-  }
-  return isData(obj, key);
+	if (typeOf(obj) !== 'object') {
+		return false;
+	}
+	if ('get' in obj) {
+		return isAccessor(obj, key);
+	}
+	return isData(obj, key);
 };
--- test/index.js
+++ test/index.js
@@ -3,92 +3,92 @@
 require('mocha');
 var assert = require('assert');
 var isDescriptor = require('../');
-var noop = function() {};
+var noop = function () {};
 
-describe('isDescriptor', function() {
-  describe('value type', function() {
-    it('should be false when not an object:', function() {
-      assert(!isDescriptor('a'));
-      assert(!isDescriptor(null));
-      assert(!isDescriptor([]));
-    });
-  });
+describe('isDescriptor', function () {
+	describe('value type', function () {
+		it('should be false when not an object:', function () {
+			assert(!isDescriptor('a'));
+			assert(!isDescriptor(null));
+			assert(!isDescriptor([]));
+		});
+	});
 
-  describe('check for descriptor', function() {
-    it('should return true if the property exists', function() {
-      var obj = {};
-      obj.foo = null;
+	describe('check for descriptor', function () {
+		it('should return true if the property exists', function () {
+			var obj = {};
+			obj.foo = null;
 
-      Object.defineProperty(obj, 'bar', {
-        value: 'xyz'
-      });
+			Object.defineProperty(obj, 'bar', {
+				value: 'xyz'
+			});
 
-      Object.defineProperty(obj, 'baz', {
-        get: function() {
-          return 'aaa';
-        }
-      });
+			Object.defineProperty(obj, 'baz', {
+				get: function () {
+					return 'aaa';
+				}
+			});
 
-      assert(isDescriptor(obj, 'foo'));
-      assert(isDescriptor(obj, 'bar'));
-      assert(isDescriptor(obj, 'baz'));
-    });
-  });
+			assert(isDescriptor(obj, 'foo'));
+			assert(isDescriptor(obj, 'bar'));
+			assert(isDescriptor(obj, 'baz'));
+		});
+	});
 
-  describe('data descriptor:', function() {
-    it('should be false when the object has invalid properties:', function() {
-      assert(!isDescriptor({value: 'foo', get: noop}));
-      assert(!isDescriptor({get: noop, value: noop}));
-    });
+	describe('data descriptor:', function () {
+		it('should be false when the object has invalid properties:', function () {
+			assert(!isDescriptor({ value: 'foo', get: noop }));
+			assert(!isDescriptor({ get: noop, value: noop }));
+		});
 
-    it('should not be false when the object has unrecognize properties:', function() {
-      assert(isDescriptor({value: 'foo', bar: 'baz'}));
-      assert(isDescriptor({value: 'foo', bar: 'baz'}));
-    });
+		it('should not be false when the object has unrecognize properties:', function () {
+			assert(isDescriptor({ value: 'foo', bar: 'baz' }));
+			assert(isDescriptor({ value: 'foo', bar: 'baz' }));
+		});
 
-    it('should be true when the object has valid properties:', function() {
-      assert(isDescriptor({value: 'foo'}));
-      assert(isDescriptor({value: noop}));
-    });
+		it('should be true when the object has valid properties:', function () {
+			assert(isDescriptor({ value: 'foo' }));
+			assert(isDescriptor({ value: noop }));
+		});
 
-    it('should be false when a value is not the correct type:', function() {
-      assert(!isDescriptor({value: 'foo', enumerable: 'foo'}));
-      assert(!isDescriptor({value: 'foo', configurable: 'foo'}));
-      assert(!isDescriptor({value: 'foo', writable: 'foo'}));
-    });
-  });
+		it('should be false when a value is not the correct type:', function () {
+			assert(!isDescriptor({ value: 'foo', enumerable: 'foo' }));
+			assert(!isDescriptor({ value: 'foo', configurable: 'foo' }));
+			assert(!isDescriptor({ value: 'foo', writable: 'foo' }));
+		});
+	});
 
-  describe('accessor descriptor:', function() {
-    it('should be false when the object has invalid properties:', function() {
-      assert(!isDescriptor({get: noop, writable: true}));
-      assert(!isDescriptor({get: noop, value: true}));
-    });
+	describe('accessor descriptor:', function () {
+		it('should be false when the object has invalid properties:', function () {
+			assert(!isDescriptor({ get: noop, writable: true }));
+			assert(!isDescriptor({ get: noop, value: true }));
+		});
 
-    it('should not be false when the object has unrecognize properties:', function() {
-      assert(isDescriptor({get: noop, set: noop, bar: 'baz'}));
-    });
+		it('should not be false when the object has unrecognize properties:', function () {
+			assert(isDescriptor({ get: noop, set: noop, bar: 'baz' }));
+		});
 
-    it('should be false when an accessor is not a function:', function() {
-      assert(!isDescriptor({get: noop, set: 'baz'}));
-      assert(!isDescriptor({get: 'foo', set: noop}));
-      assert(!isDescriptor({get: 'foo', bar: 'baz'}));
-      assert(!isDescriptor({get: 'foo', set: 'baz'}));
-    });
+		it('should be false when an accessor is not a function:', function () {
+			assert(!isDescriptor({ get: noop, set: 'baz' }));
+			assert(!isDescriptor({ get: 'foo', set: noop }));
+			assert(!isDescriptor({ get: 'foo', bar: 'baz' }));
+			assert(!isDescriptor({ get: 'foo', set: 'baz' }));
+		});
 
-    it('should be false when "get" is not a function', function() {
-      assert(!isDescriptor({set: noop}));
-      assert(!isDescriptor({get: 'foo'}));
-    });
+		it('should be false when "get" is not a function', function () {
+			assert(!isDescriptor({ set: noop }));
+			assert(!isDescriptor({ get: 'foo' }));
+		});
 
-    it('should be true when the object has valid properties:', function() {
-      assert(isDescriptor({get: noop, set: noop}));
-      assert(isDescriptor({get: noop}));
-    });
+		it('should be true when the object has valid properties:', function () {
+			assert(isDescriptor({ get: noop, set: noop }));
+			assert(isDescriptor({ get: noop }));
+		});
 
-    it('should be false when a value is not the correct type:', function() {
-      assert(!isDescriptor({get: noop, set: noop, enumerable: 'foo'}));
-      assert(!isDescriptor({set: noop, configurable: 'foo'}));
-      assert(!isDescriptor({get: noop, configurable: 'foo'}));
-    });
-  });
+		it('should be false when a value is not the correct type:', function () {
+			assert(!isDescriptor({ get: noop, set: noop, enumerable: 'foo' }));
+			assert(!isDescriptor({ set: noop, configurable: 'foo' }));
+			assert(!isDescriptor({ get: noop, configurable: 'foo' }));
+		});
+	});
 });
