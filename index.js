9fd1ac80cd42600510dc76de74da9a3834c4358d
Jordan Harband <ljharb@gmail.com>
2023-10-26 11:14:19 -0700

    [Fix] allow any non-primitive; arrays and functions are objects too

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var typeOf = require('kind-of');
 var isAccessor = require('is-accessor-descriptor');
 var isData = require('is-data-descriptor');
 
 module.exports = function isDescriptor(obj, key) {
-	if (typeOf(obj) !== 'object') {
+	if (!obj || (typeof obj !== 'object' && typeof obj !== 'function')) {
 		return false;
 	}
 
