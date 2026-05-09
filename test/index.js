40a7b8ec7371e9b5a2287bcc37a51654fad90279
Jordan Harband <ljharb@gmail.com>
2026-05-09 13:25:39 -0700

    [Fix] isolate throwing accessors (c1d7d88246a8aec0806288f75a63753ee58bc9a6)

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var test = require('tape');
+var hasProto = require('has-proto')();
+
 var isDescriptor = require('../');
+
 var noop = function () {};
 
 test('isDescriptor', function (t) {
@@ -110,6 +113,44 @@ test('isDescriptor', function (t) {
 
 			s2t.end();
 		});
+
+		st.end();
+	});
+
+	t.test('null-prototype objects', { skip: !hasProto }, function (st) {
+		var obj = { __proto__: null, foo: 1 };
+		st.equal(isDescriptor(obj, 'foo'), true, 'own data property on a null-prototype object is a descriptor');
+		st.equal(
+			isDescriptor(obj, 'missing'),
+			false,
+			'missing key on a null-prototype object returns false (not undefined or throw)'
+		);
+		st.equal(typeof isDescriptor(obj, 'missing'), 'boolean', 'returns a strict boolean');
+
+		st.end();
+	});
+
+	t.test('hostile descriptor input: throwing accessors do not propagate', function (st) {
+		var configThrow = Object.defineProperty(
+			{ value: 1, writable: false },
+			'configurable',
+			{ enumerable: true, get: function () { throw new Error('configurable'); } }
+		);
+		st.equal(isDescriptor(configThrow), false, 'throwing enumerable .configurable getter returns false');
+
+		var enumThrow = Object.defineProperty(
+			{ value: 1, writable: false },
+			'enumerable',
+			{ enumerable: true, get: function () { throw new Error('enumerable'); } }
+		);
+		st.equal(isDescriptor(enumThrow), false, 'throwing enumerable .enumerable getter returns false');
+
+		st.end();
+	});
+
+	t.test('hostile Proxy `has` trap does not propagate', { skip: typeof Proxy !== 'function' }, function (st) {
+		var bomb = new Proxy({}, { has: function () { throw new Error('has'); } });
+		st.equal(isDescriptor(bomb, 'foo'), false, 'throwing Proxy `has` trap returns false');
 
 		st.end();
 	});
